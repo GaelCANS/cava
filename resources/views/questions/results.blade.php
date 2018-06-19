70736f8db4f0764b3bccb3ae4c5a8bf93e72a453
Gael <gael.levant@ca-normandie-seine.fr>
2018-06-19 14:51:42 +0200

    cleaning code

--- resources/views/questions/results.blade.php
+++ resources/views/questions/results.blade.php
@@ -5,15 +5,12 @@
         <div class="col-md-8 text-center mx-auto px-4 mb-5">
             <h3>{{$survey->Blueprint->name}}</h3>
             Notes moyennes
-
         </div>
     </div>
 
     @foreach($averages as $average)
         @include('questions.average')
     @endforeach
-
-
 
     @include('questions.modal')
 
