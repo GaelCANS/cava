ad15a98ac5bff35bda3e50a54209dfc3d23c7105
masseaxe <masse.axel@gmail.com>
2018-06-20 17:25:18 +0200

    graphic font others

--- resources/views/questions/results.blade.php
+++ resources/views/questions/results.blade.php
@@ -3,8 +3,9 @@
 @section('content')
     <div class="row">
         <div class="col-md-8 text-center mx-auto px-4 mb-5">
-            <h3>{{$survey->Blueprint->name}}</h3>
-            Notes moyennes
+            <div class="row>"><h3>{{$survey->Blueprint->name}}</h3></div>
+            Notes moyennes sur le période actuelle
+            Note moyenne toutes périodes
         </div>
     </div>
 
