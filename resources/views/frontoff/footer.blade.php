ab7de80f28c18674834487180e2ea8b616051157
masseaxe <masse.axel@gmail.com>
2018-07-09 10:56:37 +0200

    graphic details

--- resources/views/frontoff/footer.blade.php
+++ resources/views/frontoff/footer.blade.php
@@ -1,3 +1,3 @@
 <div class="footer p-4 text-center">
-    <img src="{{ asset('/img/cans-logo-grey.png') }}" alt="Crédit Agricole Normandie-Seine" width="240" />
+    <img src="{{ asset('/public/img/cans-logo-grey.png') }}" alt="Crédit Agricole Normandie-Seine" width="240" />
 </div>
--- resources/views/questions/close.blade.php
+++ resources/views/questions/close.blade.php
@@ -1,15 +1,19 @@
-<div class="row justify-content-between col-md-8 mx-auto">
+<div class="row justify-content-between col-md-8 mx-auto p-0">
     <div class="head-range">
         <span class="float-left mb-3 d-inline-block">Pas du tout</span>
         <span class="float-right mb-3  d-inline-block">Totalement</span>
     </div>
     <input type="range" min="0" max="5" step="1" name="result" data-first="@if ($result == -1){{1}}@else{{0}}@endif" id="result-range" value="{{ $result }}"  >
-    <datalist>
+    <!--<datalist>
         <option value="0" style="background: transparent;">
         <option value="1">
         <option value="2">
         <option value="3">
         <option value="4">
         <option value="5"style="background: transparent;">
-    </datalist>
+    </datalist>-->
+    <span class="barGraph-check top40"></span>
+    <span class="barGraph-check ml40 top40"></span>
+    <span class="barGraph-check ml60 top40"></span>
+    <span class="barGraph-check ml80 top40"></span>
 </div>
