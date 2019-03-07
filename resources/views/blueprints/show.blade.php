3d98955d27ebe5d8efe556c77582fe83d4efbbef
Gael <gael.levant@ca-normandie-seine.fr>
2019-03-07 10:59:48 +0100

    Nav tab on blueprint

--- resources/views/blueprints/show.blade.php
+++ resources/views/blueprints/show.blade.php
@@ -14,12 +14,12 @@
     </div>
 
     <ul class="nav nav-tabs">
-        <li class="@if($tab == 'blueprint') active @endif"><a href="@if($tab != 'blueprint'){{action("BlueprintController@show" , $blueprint)}}@endif">Sondage</a></li>
-        <li class="@if( $blueprint == null ) disabled @endif @if($tab == 'questions') active @endif"><a href="@if( $blueprint == null ){{'#'}}@else{{route('list-question',$blueprint)}}@endif">Questions</a></li>
-        <li class="@if( $blueprint == null ) disabled @endif @if($tab == 'guests') active @endif"><a href="@if( $blueprint == null ){{'#'}}@else{{route('list-users' , $blueprint)}}@endif">Utilisateurs</a></li>
-        <li class="@if( $blueprint == null ) disabled @endif @if($tab == 'surveys') active @endif"><a href="@if( $blueprint == null ){{'#'}}@else{{route('list-survey' , $blueprint)}}@endif">Itérations</a></li>
+        <li class="nav-item @if($tab == 'blueprint') active @endif"><a class="nav-link @if($tab == 'blueprint') active @endif" href="@if($tab != 'blueprint'){{action("BlueprintController@show" , $blueprint)}}@endif">Sondage</a></li>
+        <li class="nav-item @if( $blueprint == null ) disabled @endif @if($tab == 'questions') active @endif"><a class="nav-link @if($tab == 'questions') active @endif"  href="@if( $blueprint == null ){{'#'}}@else{{route('list-question',$blueprint)}}@endif">Questions</a></li>
+        <li class="nav-item @if( $blueprint == null ) disabled @endif @if($tab == 'guests') active @endif"><a class="nav-link @if($tab == 'guests') active @endif"  href="@if( $blueprint == null ){{'#'}}@else{{route('list-users' , $blueprint)}}@endif">Utilisateurs</a></li>
+        <li class="nav-item @if( $blueprint == null ) disabled @endif @if($tab == 'surveys') active @endif"><a class="nav-link @if($tab == 'surveys') active @endif"  href="@if( $blueprint == null ){{'#'}}@else{{route('list-survey' , $blueprint)}}@endif">Itérations</a></li>
         @if ($blueprint->SpeLN)
-        <li class="@if( $blueprint == null ) disabled @endif @if($tab == 'pilotage') active @endif"><a href="@if( $blueprint == null ){{'#'}}@else{{route('pilotage' , $blueprint)}}@endif">Statistiques</a></li>
+        <li class="nav-item @if( $blueprint == null ) disabled @endif @if($tab == 'pilotage') active @endif"><a class="nav-link @if($tab == 'pilotage') active @endif"  href="@if( $blueprint == null ){{'#'}}@else{{route('pilotage' , $blueprint)}}@endif">Statistiques</a></li>
         @endif
     </ul>
 
