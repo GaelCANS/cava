609d675c3ef48cec6a433c7155f79f13101400d5
Gael <gael.levant@ca-normandie-seine.fr>
2019-04-10 16:35:32 +0200

    fix on result

--- resources/views/questions/average.blade.php
+++ resources/views/questions/average.blade.php
@@ -10,7 +10,7 @@
         </div>
         <div style="clear:both;"></div>
         <div class="col-md-12">
-            <h4 class="mt-3 mb-4" id="wording-{{$average['key']}}">{{utf8_decode($average['wording'])}}</h4>
+            <h4 class="mt-3 mb-4" id="wording-{{$average['key']}}">{{($average['wording'])}}</h4>
             <div class="barGraph">
                 <ul class="graph">
                   <span class="graph-barBack">
