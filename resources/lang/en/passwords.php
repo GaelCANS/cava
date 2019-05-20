72b8f28c95fa534c71e92f283d4ecf7a6dca0cac
Gael <gael.levant@ca-normandie-seine.fr>
2019-05-20 14:59:30 +0200

    translate reset password

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -15,8 +15,8 @@ return [
 
     'password' => 'Passwords must be at least six characters and match the confirmation.',
     'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
+    'sent' => 'Nous avons envoyé votre lien de réinitialisation de mot de passe par e-mail!',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'user' => "Nous avons envoyé votre lien de réinitialisation de mot de passe par e-mail!",
 
 ];
